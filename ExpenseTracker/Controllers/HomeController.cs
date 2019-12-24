9dcd81a64f1d4e58c0604e94a11390ea45567345
Jonathan Kilpatrick <kilpatrick.jonluc@outlook.com>
2019-12-24 07:58:50 -0500

    Add project files.

---
+++ ExpenseTracker/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+      
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public IActionResult Index(string message)
+        {
+            ViewBag.Message = message;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult CreateUser()
+        {
+            
+            return View("CreateUser", new User());
+        }
+        [HttpPost]
+        public IActionResult CreateUser(User user)
+        {
+            bool isError = false;
+            if (user.FirstName != "Jon")
+            {
+                ModelState.AddModelError("FirstName", "First Name Must Be Jon");
+                isError = true;
+            }
+            if (user.SSN == null)
+            {
+                ModelState.AddModelError("SSN", "SSN must be significant");
+                isError = true;
+            }
+            if (!isError)
+            {
+                return View("CreateUser", user);
+            }
+            else
+            {
+                return View("Index", "Thank you! Acount Created!");
+            }
+        }
+    }
+}
---
+++ ExpenseTracker/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Models
+{
+    public class User
+    {
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Email")]
+        public string email { get; set; }
+        
+        [DisplayName("User Name")]
+        public string userName { get; set; }
+
+        [DisplayName("Password")]
+        public string password { get; set; }
+        
+        [DisplayName("Phone Number")]
+        public string phoneNumber { get; set; }
+        
+        [DisplayName("Social Security Number")]
+        public string SSN { get; set; }
+    }
+}
