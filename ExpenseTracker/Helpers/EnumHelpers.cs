f53e4069800def7c7176f9faa08b788f331570b7
michael_townsend <michael.h.townsend@gmail.com>
2020-05-24 23:40:27 -0400

    Cleaned up code

--- ExpenseTracker/Helpers/EnumHelpers.cs
+++ ExpenseTracker/Helpers/EnumHelpers.cs
@@ -9,20 +9,6 @@ namespace ExpenseTracker.Helpers
 {
     public static class EnumHelpers<T>
     {
-        //public static string GetDisplayValue(T value)
-        //{
-        //    var fieldInfo = value.GetType().GetField(value.ToString());
-
-        //    var descriptionAttributes = fieldInfo.GetCustomAttributes(
-        //        typeof(DisplayAttribute), false) as DisplayAttribute[];
-
-        //    if (descriptionAttributes[0].ResourceType != null)
-        //        return LookupResource(descriptionAttributes[0].ResourceType, descriptionAttributes[0].Name);
-
-        //    if (descriptionAttributes == null) return string.Empty;
-        //    return (descriptionAttributes.Length > 0) ? descriptionAttributes[0].Name : value.ToString();
-        //}
-
         public static string GetDescription(T value)
         {
             var fieldInfo = value.GetType().GetField(value.ToString());
@@ -42,18 +28,5 @@ namespace ExpenseTracker.Helpers
             }
         }
 
-        //private static string LookupResource(Type resourceManagerProvider, string resourceKey)
-        //{
-        //    foreach (PropertyInfo staticProperty in resourceManagerProvider.GetProperties(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public))
-        //    {
-        //        if (staticProperty.PropertyType == typeof(System.Resources.ResourceManager))
-        //        {
-        //            System.Resources.ResourceManager resourceManager = (System.Resources.ResourceManager)staticProperty.GetValue(null, null);
-        //            return resourceManager.GetString(resourceKey);
-        //        }
-        //    }
-
-        //    return resourceKey; // Fallback with the key name
-        //}
     }
 }
